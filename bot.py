0522c57f2b342460aea81e9d6581a4e6285c8dae
Bogdan <bogdan.evstratenko@onetwotrip.com>
2016-11-24 11:41:39 +0300

    db conn fix

--- bot.py
+++ bot.py
@@ -1,21 +1,19 @@
-import logging
-import os
-from collections import OrderedDict
-from datetime import datetime as dt
-from itertools import zip_longest
-
+from telegram import ReplyKeyboardMarkup, ParseMode
+from emoji import emojize
+from telegram.ext import Updater, CommandHandler, RegexHandler, MessageHandler, Filters, ConversationHandler
+from config import BUYPROTECT, ALLTESTS
 from pyexcel_xlsx import get_data, save_data
-from telegram import ParseMode
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
-
-from config import BUYPROTECT
+from itertools import zip_longest
+from collections import OrderedDict
+import logging
+from datetime import datetime as dt
+import os
 from model import save, Users, \
-    UndefinedRequests, Company, Good, Service, Aliases, DoesNotExist, fn
-
+    UndefinedRequests, Company, Good, Service, Aliases, DoesNotExist, fn, \
+    before_request_handler, after_request_handler, JOIN
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logging.basicConfig(filename='logs.log', filemode='w+', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.DEBUG)
+logging.basicConfig(filename='logs.log', filemode='w+', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
 ADMINS = [209743126, 56631662, 214688324]
 
@@ -55,12 +53,15 @@ search_fckup_msg = '''Мы не нашли совпадений, но приня
 
 
 def unknown_req_add(tid, txt):
+    before_request_handler()
     try:
         UndefinedRequests.get(fn.lower(UndefinedRequests.request) == txt.lower())
         UndefinedRequests.create(from_user=tid, request=txt)
     except DoesNotExist:
         UndefinedRequests.create(from_user=tid, request=txt)
+        after_request_handler()
         return True
+    after_request_handler()
     return False
 
 
@@ -74,9 +75,11 @@ def start(bot, update):
           'Для поиска просто введи название компании, рекламного ролика, товара или фразы из рекламы. ' \
           'Если информации не будет в базе, мы получим твой запрос и организуем проверку'
     try:
+        before_request_handler()
         Users.get(Users.telegram_id == uid)
     except DoesNotExist:
         Users.create(telegram_id=uid, username=username, name=name)
+    after_request_handler()
     if uid in ADMINS:
         bot.sendMessage(uid, msg)
         return
@@ -91,16 +94,16 @@ def search_wo_cat(bot, update):
     res = []
     msg = ''
     try:
-        check_aliases = (Aliases.select().where((Aliases.alias1 == message) |
-                                                (Aliases.alias2 == message) |
-                                                (Aliases.alias3 == message) |
-                                                (Aliases.alias4 == message) |
-                                                (Aliases.alias5 == message) |
-                                                (Aliases.alias6 == message) |
-                                                (Aliases.alias7 == message) |
-                                                (Aliases.alias8 == message) |
-                                                (Aliases.alias9 == message) |
-                                                (Aliases.alias10 == message))).execute()
+        check_aliases = (Aliases.select().where((fn.lower(Aliases.alias1) == message) |
+                                                (fn.lower(Aliases.alias2) == message) |
+                                                (fn.lower(Aliases.alias3) == message) |
+                                                (fn.lower(Aliases.alias4) == message) |
+                                                (fn.lower(Aliases.alias5) == message) |
+                                                (fn.lower(Aliases.alias6) == message) |
+                                                (fn.lower(Aliases.alias7) == message) |
+                                                (fn.lower(Aliases.alias8) == message) |
+                                                (fn.lower(Aliases.alias9) == message) |
+                                                (fn.lower(Aliases.alias10) == message))).execute()
         alias = [c.key for c in check_aliases]
         if alias:
             message = alias[0]
@@ -110,11 +113,13 @@ def search_wo_cat(bot, update):
     for model in dbs.values():
         if model == Aliases:
             continue
+        before_request_handler()
         try:
             search = model.get(fn.lower(model.name) == message.lower())
             res.append(search)
         except DoesNotExist:
             pass
+        after_request_handler()
     if not res:
         if unknown_req_add(uid, message.strip('"\'!?[]{},. ')):
             bot.sendMessage(uid, search_fckup_msg, disable_web_page_preview=True)
@@ -138,13 +143,12 @@ def process_file(bot, update):
         columns = ('name', 'description', 'url')
         for sheet in sheets:
             if sheet.lower() == 'алиасы':
-                columns = ['key', 'alias1', 'alias2', 'alias3', 'alias4', 'alias5', 'alias6', 'alias7', 'alias8',
-                           'alias9', 'alias10']
+                columns = ['key', 'alias1', 'alias2', 'alias3', 'alias4', 'alias5', 'alias6', 'alias7', 'alias8', 'alias9', 'alias10']
             _data = []
             for row in sheets[sheet][1:]:
                 if not row:
                     continue
-                _data.append(dict(zip_longest(columns, [r.strip('"\'!?[]{},. \n').lower() for r in row], fillvalue='')))
+                _data.append(dict(zip_longest(columns, [r.strip('"\'!?[]{},. \n') for r in row], fillvalue='')))
             if save(_data, dbs[sheet]):
                 bot.sendMessage(uid, 'Данные на странице {} сохранил'.format(sheet), disable_notification=1)
             else:
@@ -158,11 +162,13 @@ def search(bot, update):
     message = update.message.text.strip('"\'!?[]{},. ')
     res = []
     msg = ''
+    before_request_handler()
     try:
         Model = user_search.pop(uid)
         search = Model.get(fn.lower(Model.name) == message.lower())
         res.append((search.name, search.description, search.url))
     except DoesNotExist:
+        after_request_handler()
         if unknown_req_add(uid, message.strip('"\'!?[]{},. ')):
             bot.sendMessage(uid, search_fckup_msg, disable_web_page_preview=True)
         else:
@@ -181,8 +187,10 @@ def output(bot, update):
     if uid not in ADMINS:
         return
     foud = OrderedDict()
-    res = UndefinedRequests.select(UndefinedRequests.request, fn.COUNT(UndefinedRequests.id).alias('count')). \
+    before_request_handler()
+    res = UndefinedRequests.select(UndefinedRequests.request, fn.COUNT(UndefinedRequests.id).alias('count')).\
         group_by(UndefinedRequests.request).execute()
+    after_request_handler()
     foud.update({'Отсутствия в базе': [(r.request, r.count) for r in res]})
     fname = str(dt.now()) + '.xlsx'
     save_data(fname, foud)
@@ -199,3 +207,4 @@ dp.add_handler(MessageHandler(Filters.text, search_wo_cat))
 dp.add_handler(MessageHandler(Filters.document, process_file))
 updater.start_polling()
 updater.idle()
+
